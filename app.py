ef4331e1236649d717855996ff6ac38c82cb1770
yashrajrawat004 <yashrajrawat004@gmail.com>
2024-12-01 09:45:13 +0530

    Data Ingestion Completed

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+import sys
 from src.mlproject.logger import logging
+from src.mlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
+from src.mlproject.exception import CustomException
 
 if __name__=='__main__':
-    logging.info('Execution Started')
-    logging.info('Project Start From Here')
+    logging.info('Testing Custom Exception')
+        
+    try:
+        #data_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info(CustomException(e,sys))
+        raise CustomException(e,sys)
---
+++ src/mlproject/utils.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import pickle
+import pymysql
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from sklearn.metrics import r2_score
+from src.mlproject.logger import logging
+from sklearn.model_selection import GridSearchCV
+from src.mlproject.exception import CustomException
+
+# Loading mysql cofigs
+load_dotenv()
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv('db')
+
+# Reading data from mysql
+def read_sql_data():
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection Established",mydb)
+        df=pd.read_sql_query('Select * from diabetes',mydb)
+        logging.info("Fetching Successful",mydb)
+        return df
+    
+    except Exception as ex:
+        raise CustomException(ex)
+    
+    
+    
+    
+"""def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)"""
